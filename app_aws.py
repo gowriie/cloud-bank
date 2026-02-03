850dd081493cabc13e4e481b7b13a21eb9ee7ca7
Gowri S <cgsgrs@gmail.com>
2026-02-03 19:54:23 +0530

    ARN updated

--- app_aws.py
+++ app_aws.py
@@ -22,7 +22,7 @@ USERS_TABLE_NAME = os.environ.get("USERS_TABLE_NAME", "CloudBankUsers")
 TX_TABLE_NAME = os.environ.get("TX_TABLE_NAME", "CloudBankTransactions")
 ALERTS_TABLE_NAME = os.environ.get("ALERTS_TABLE_NAME", "CloudBankAlerts")
 
-SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:412381764362:cloud_bank_topic"
+SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:345594607481:cloud_bank_topic"
 
 dynamodb = boto3.resource("dynamodb", region_name=REGION)
 users_table = dynamodb.Table(USERS_TABLE_NAME)
